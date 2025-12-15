1a2af3208ee8eab0d8beb46f9a4386dfc152831d
almamunrasel <raselalmamun096@gmail.com>
2025-12-15 23:58:25 +0600

    resolved task

--- src/Components/ResolvedTask/ResolvedTask.jsx
+++ src/Components/ResolvedTask/ResolvedTask.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+
 const ResolvedTask = ({resolv}) => {
   return (
-    <div className='border border-gray-100 rounded-xl p-3 font-semibold text-lg bg-[#E0E7FF] mt-2'>
+    <div className='border border-gray-100 rounded-xl p-3  text-lg bg-green-200 mt-2'>
       <h1 className='text-center'>{resolv.title}</h1>
+      <h2 className='text-center text-green-700 font-bold'>Completed</h2>
       
       
     </div>
